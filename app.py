08fda3342b182adb2e96cf57cda1b63a028b61ef
Oliver Marcell Toth <68820833+olivermtoth@users.noreply.github.com>
2023-05-16 15:39:38 +0000

    Stock feuture

--- app.py
+++ app.py
@@ -2,12 +2,21 @@
 Main application file.
 Run as 'flask run' from the terminal
 """
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import sqlite3 as sql
+from stock import Stock
 
+balance = 100_000
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def hello():
-    return render_template('index.html')
+    if request.method == "GET":
+        return render_template('index.html', balance=balance)
+    else:
+        print("Form submitted")
+        stock = request.form.get("stock")
+        s = Stock(stock)
+        data = s.make_json()
+        return render_template("stock.html", stock=stock, data=data, balance=balance)
