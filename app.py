60270edbc5448b8cd353e795704abe32f8da72d3
Oliver Marcell Toth <oliverm.toth@protonmail.ch>
2023-05-18 16:44:46 -0400

    add chart

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from flask import Flask, render_template, request
 import sqlite3 as sql
 from stock import Stock
 
-balance = 100_000
+balance = 100_000.00
 
 app = Flask(__name__)
 
@@ -16,7 +16,11 @@ def hello():
         return render_template('index.html', balance=balance)
     else:
         print("Form submitted")
-        stock = request.form.get("stock")
-        s = Stock(stock)
-        data = s.make_json()
-        return render_template("stock.html", stock=stock, data=data, balance=balance, price= s.current_price)
+        try:
+            stock = request.form.get("stock")
+            s = Stock(stock)
+            # data = s.make_json()
+            return render_template("stock.html", stock=stock, balance=balance, price= s.current_price)
+        except KeyError:
+            print("Key Error raised, directing to the error page")
+            return render_template("tickererror.html", balance=balance)
